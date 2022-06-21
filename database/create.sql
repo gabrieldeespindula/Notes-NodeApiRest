e8be7dd92c1529c0c513e843effe66b19f2dcf62
gabrieldeespindula <gabrieldeespindula80@gmail.com>
2022-06-20 21:24:25 -0300

    Using dotenv to keep information secure

--- database/create.sql
+++ database/create.sql
@@ -1,18 +1,18 @@
 create schema notes;
 
-create table notes.note (
-	id serial primary key,
-	title text not null,
-	content text not null,
-	user_id integer not null,
-	date timestamp default now()
-	foreign key (user_id) references notes.user (id)
-);
-
 create table notes.user (
 	id serial primary key,
 	name text not null,
 	email text not null UNIQUE,
 	password text not null,
 	date timestamp default now()
+);
+
+create table notes.note (
+	id serial primary key,
+	title text not null,
+	content text not null,
+	user_id integer not null,
+	date timestamp default now(),
+	foreign key (user_id) references notes.user (id)
 );
--- server/environment/environment.js
+++ server/environment/environment.js
@@ -1,14 +1,27 @@
+require('dotenv').config();
+
 const environment = {
 	url: 'http://localhost:3000/',
 	db: {
-		user: 'postgres',
-		password: '1234',
-		host: 'localhost',
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		host: process.env.DB_HOST,
 		port: 5432,
 		schema: 'notes',
-		database: 'postgres'
+		database: process.env.DB_DATABASE
 	},
-	JWT_KEY: "jpt6kjd@yrt5qvc!PYD"
+	JWT_KEY: process.env.JWT_KEY
 };
+
+// Host
+// ec2-54-147-33-38.compute-1.amazonaws.com
+// Database
+// d99uoeq7bif2md
+// User
+// yqlttwvlwtxawg
+// Port
+// 5432
+// Password
+// 4e6fd64a9ce58a3e41f436abac565f589d5f761d0956b62245951f4c98b7b488
 
 module.exports = environment;
